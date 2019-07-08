cf75fd5557a2b181f8a5f48bcad36463dc3079fa
marcogallo <mgalloperalta@gmail.com>
2019-07-08 16:09:49 +0200

    type and # methods

--- luatut.lua
+++ luatut.lua
@@ -15,6 +15,11 @@ In this case a string
 name = "marco";
 secondName = 'jose';
 -- io.write() is another way to print stuff to terminal
-io.write("size of firstname is: ", #name, " and the size of the second name is: ", #secondName);
+-- "\n") is used to print in a new line
+-- #return the variables length
+io.write("size of firstname is: ", #name, " and the size of the second name is: ", #secondName, "\n");
+
+--type methods return the variable type
+print(type(name));
 
 
