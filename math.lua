7e8504f9ab8ed6b0095a48d3406aff1a51693572
marcogallo <mgalloperalta@gmail.com>
2019-07-08 17:21:12 +0200

    Add description to math methods

--- math.lua
+++ math.lua
@@ -3,5 +3,28 @@ io.write("5 - 3 = ", 5 - 3, "\n" );
 io.write("5 / 3 = ", 5 / 3, "\n"  );
 io.write("5.2 % 3 = ", 5.2 % 3, "\n" );
 io.write("5 * 3 = ", 5 * 3, "\n" );
+io.write("2 - (5 * 3) = ",2 - (5 * 3), "\n" );
+
+--[[math methods:
+  math.abs = Return the absolute, or non-negative value, of a given value.,
+  math.acos /  math.asin = Return the inverse cosine and sine in radians of the given value.
+  math.atan = Return the inverse tangent in radians. We can do this by supplying y/x ourselves or we can pass y and x to math.atan to do this for us.
+  math.ceil /math.floor = Return the integer no greater than or no less than the given value (even for negatives).
+  math.cos / math.sin /  math.tan = Return the cosine, sine and tangent value for a given value in radians.
+  math.deg / math.rad = Convert from radians to degrees and vice versa.
+  math.exp / math.log = math.exp(myval) returns e (the base of natural logarithms) raised to the power myval. math.log() returns the inverse of this. math.exp(1) returns e.
+  math.fmod 
+  math.huge = math.huge is a constant. It represents +infinity.
+  math.max / math.min = Return the minimum or maximum value from a variable length list of arguments.
+  math.maxinteger
+  math.mininteger
+  math.modf = Return the integral and fractional parts of the given number.
+  math.pi = This is a part of the constant Pi.
+  math.random / math.randomseed = math.random() generates pseudo-random numbers uniformly distributed. Supplying argument alters its behaviour:
+  math.sqrt = Return the square root of a given number. Only non-negative arguments are allowed.
+  math.tointeger
+  math.type
+  math.ult
+]]
 
 
