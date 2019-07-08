8d8b43b789e36e0e125e39fd8827717a8b8f4966
marcogallo <mgalloperalta@gmail.com>
2019-07-08 16:04:44 +0200

    Practice variable creation and console printing.

--- luatut.lua
+++ luatut.lua
@@ -1,1 +1,20 @@
+
+--[[
+  this is how you do multiline comments
+    vklvdlkgdlkfgd
+]]
+
+-- print function is sort of the console log (one line comment)
 print ("Hello world!");
+
+--[[
+declaring variables. Lua dinamically assigns the type to the variable.
+In this case a string
+]] 
+
+name = "marco";
+secondName = 'jose';
+-- io.write() is another way to print stuff to terminal
+io.write("size of firstname is: ", #name, " and the size of the second name is: ", #secondName);
+
+
